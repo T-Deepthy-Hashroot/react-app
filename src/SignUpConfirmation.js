a6787d8c3edf3a772846b6dc17a2ca92692f39e5
T-Deepthy <deeps1929@gmail.com>
2019-10-03 17:59:22 +0530

    Console params in signup confirmation

--- src/SignUpConfirmation.js
+++ src/SignUpConfirmation.js
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
 import React from 'react';
 class SignUpConfirmation extends React.Component { 
     constructor(props){
         super(props);
+        alert(this.props);
         this.changeInTextField = this.changeInTextField.bind(this);
         
     }
@@ -9,14 +11,18 @@ class SignUpConfirmation extends React.Component {
          this.props.valueChange(event.target.name , event.target.value)
     }
     render() { 
+        console.log("signupconfirmation",this.props);
+        console.log("params confirmationtoken", this.props.match.params.confirmationtoken);
         return ( 
                 <div>
-                    <label>{this.props.label}</label>
-                  <input name={this.props.name} type={this.props.type} className="form-control col-md-9" value={this.props.value} placeholder={this.props.placeholder}
-                   onChange={this.changeInTextField} /> 
+                    <p>Account activated!</p>
+                    <Link to="login" className="ml-2 mt-2">Login</Link>   
                 </div>   
   
         )
     }
 }
+
+
+
 export default SignUpConfirmation;
