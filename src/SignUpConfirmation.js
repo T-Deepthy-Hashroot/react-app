28b55999309055d89d53abfe6bf24ba3a187f319
T-Deepthy <deeps1929@gmail.com>
2019-10-04 11:28:18 +0530

    Modify signupconfirmation

--- src/SignUpConfirmation.js
+++ src/SignUpConfirmation.js
@@ -1,28 +1,26 @@
 import { Link } from "react-router-dom";
 import React from 'react';
+import axios from 'axios'
 class SignUpConfirmation extends React.Component { 
     constructor(props){
         super(props);
-        alert(this.props);
-        this.changeInTextField = this.changeInTextField.bind(this);
-        
-    }
-    changeInTextField(event){
-         this.props.valueChange(event.target.name , event.target.value)
+        axios.post('http://localhost:3001/users/confirm_signup', {
+          confirmation_token: this.props.match.params.confirmationtoken
+         })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
     }
     render() { 
-        console.log("signupconfirmation",this.props);
-        console.log("params confirmationtoken", this.props.match.params.confirmationtoken);
         return ( 
                 <div>
                     <p>Account activated!</p>
-                    <Link to="login" className="ml-2 mt-2">Login</Link>   
                 </div>   
   
         )
     }
 }
-
-
-
 export default SignUpConfirmation;
